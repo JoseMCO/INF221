e0fbd5bc97eb74882ea2d4073ce6333b70aace69
José Castro <n3ggro@gmail.com>
2016-10-03 22:54:41 -0300

    bester 1

--- Tarea1/Problema1.cpp
+++ Tarea1/Problema1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cctype>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -10,9 +9,16 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int check(vector<std::string> &map, int ncols, int nrows, int col, int row, int from, char obj);
+void joinAreas(vector< vector<int> > *areas, int currentPoint, int toReplace);
 
-int recurrence = 0;
+	// Posiciones respecto a la coordenada actual (X)
+	// +---+---+---+
+	// | 1 | 2 | 3 |
+	// +---+---+---+
+	// | 4 | X | 5 |
+	// +---+---+---+
+	// | 6 | 7 | 8 |
+	// +---+---+---+
 
 int main(void)
 {
@@ -22,135 +28,155 @@ int main(void)
 
 	while ( getline(std::cin, string) )
 	{
+		// contador para los problemas y matriz para el problema actual, 
+		// en donde -1 será tierra (L) y agua (W) estará representado por una etiqueta de área
 		problems++;
-		vector<std::string> map;
-		map.resize(1);
+		vector< vector<int> > areas;
+		vector<int> counts;
+		// contador de áreas
+		int pools = 0;
 
 		// se leen los 'mapas' con la información inicial
-		map[0] = string;
 		int lmax = string.length();	
 		int nlines = 1;
-		while ( getline(cin, string) && string.length() && !isdigit(string[0]) )
-		{
-			map.resize(++nlines);
-			map[nlines-1] = string;
-		}
 
-		// se leen las consultas sobre el mapa
-		vector< vector<int> > queries;
-		int nqueries = 1;
 		do
 		{
+			// aumenta el tamaño de areas para agregar la nueva linea
+			areas.resize(nlines);
+			areas[nlines-1].resize(lmax, -1);
+
+			// itera por la linea
+			for (int i = 0; i < lmax; i++)
+			{
+				// si la posición actual corresponde a agua
+				if (string[i] == 'W')
+				{
+					int currentPoint = -1;
+
+					// se revisa (de ser posible) las posiciones 1, 2, 3 y 4, de encontrarse, la posición actual se agrega a el área ya existente
+					// posición 1
+					if ( i>1 && nlines>1 && areas[nlines-2][i-1] > -1)
+					{
+						counts[areas[nlines-2][i-1]]++;
+						currentPoint = areas[nlines-2][i-1];
+					}
+					// posición 2
+					if ( nlines>1 && areas[nlines-2][i] > -1 && currentPoint != areas[nlines-2][i])
+					{
+						if (currentPoint == -1)
+						{
+							currentPoint = areas[nlines-2][i];
+						}
+						else
+						{
+							joinAreas(&areas, currentPoint, areas[nlines-2][i]);
+						}
+						counts[currentPoint]++;
+					}
+					// posición 3
+					if ( i<lmax-2 && nlines>1 && areas[nlines-2][i+1] > -1 && currentPoint != areas[nlines-2][i+1])
+					{
+						if (currentPoint == -1)
+						{
+							currentPoint = areas[nlines-2][i+1];
+						}
+						else
+						{
+							joinAreas(&areas, currentPoint, areas[nlines-2][i+1]);
+						}
+						counts[currentPoint]++;
+					}
+					// posición 4
+					if ( i>0 && areas[nlines-1][i-1] > -1 && currentPoint != areas[nlines-1][i-1])
+					{
+						if (currentPoint == -1)
+						{
+							currentPoint = areas[nlines-1][i-1];
+						}
+						else
+						{
+							joinAreas(&areas, currentPoint, areas[nlines-1][i-1]);
+						}
+						counts[currentPoint]++;
+					}
+
+					// de no encontrarse un área colindante, se marca el punto como una nueva área
+					if (currentPoint == -1)
+					{
+						counts.resize(++pools, 1);
+						counts[pools-1] = 1;
+						currentPoint = pools-1;
+					}
+
+					// se guarda en areas el valor corespondiente a el área
+					areas[nlines-1][i] = currentPoint;
+				}
+				else
+				{
+					areas[nlines-1][i] = -1;
+				}
+			}
+
+			// imprime paso a paso la detección de áreas y la lista con las cuentas
+			///////////////////////////////////////////////////////////////////////
+			// for (int i = 0; i < counts.size(); ++i)
+			// {
+			// 	cout << i << ": " << counts[i] << "\t";
+			// }
+			// cout << '\n';
+
+			// for (unsigned int i = 0; i < areas.size(); ++i)
+			// {
+			// 	for (unsigned int j = 0; j < areas[i].size(); ++j)
+			// 	{
+			// 		if (areas[i][j] != -1)
+			// 		{
+			// 			cout << areas[i][j] << " ";
+			// 		}
+			// 		else
+			// 		{
+			// 			cout << "- ";
+			// 		}
+			// 	}
+			// 	cout << '\n';
+			// }
+			// cout << '\n';
+			/////////////////////////////////////////////////////////////////////////
+
+			// lectura de nueva linea
+			nlines++;
+		} while ( getline(cin, string) && !isdigit(string[0]) );
+
+		// se leen las consultas sobre el mapa
+		do
+		{
+			// se parsean los 2 números
 			stringstream stream(string);
-			queries.resize(nqueries);
-			queries[nqueries-1].resize(2);
-			stream >> queries[nqueries-1][0];
-			stream >> queries[nqueries-1][1];
-			nqueries++;
-		} while ( getline(cin, string) && string.length() ); nqueries--;
-
-		// se resuelven las consultas
-		results.resize(problems);
-		results[problems-1].resize(nqueries);
-		for (int i = 0; i < nqueries; i++)
-		{
-			int col = queries[i][1]-1;
-			int row = queries[i][0]-1;
-			results[problems-1][i] = check(map, lmax, nlines, col, row, 0, map[row][col]);
-			// check(map, lmax, nlines, queries[i][1]-1, queries[i][0]-1, 0, map[queries[i][0]-1][queries[i][1]-1]);
-		}
+			int row, col;
+			stream >> row;
+			stream >> col;
+			// se consulta por el área
+			cout << counts[areas[row-1][col-1]] << "\n";
+		} while ( getline(cin, string) && string.length() ); 
+		cout << "\n";
 	}
-
-	// se entregan los resultados
-	for (int i = 0; i < problems; ++i)
-	{
-		for (int j = 0; j < results[i].size(); ++j)
-		{
-			cout << results[i][j];
-			cout << '\n';
-		}
-		cout << '\n';
-	}
-	cout << recurrence;
 
 	return 0;
 }
 
-int check(vector<std::string> &map, int ncols, int nrows, int col, int row, int from, char obj)
+// busca etiquetas toReplace y las cambia a currentPoint
+void joinAreas(vector< vector<int> > *areas, int currentPoint, int toReplace)
 {
-	recurrence++;
-	// +---+---+---+
-	// | 1 | 2 | 3 |
-	// +---+---+---+
-	// | 4 | X | 5 |
-	// +---+---+---+
-	// | 6 | 7 | 8 |
-	// +---+---+---+
-
-	// se verifica que el punto esté dentro del mapa y que corresponda a lo que busco (agua)
-	if (col >= ncols || row >= nrows || col < 0 || row < 0) return 0;
-	if (map[row][col] != obj) return 0;
-
-	// marco el punto como leído
-	map[row][col] = 'X';
-	int count = 1;
-
-	// filtro los siguientes cuadros a buscar dependiendo de quién pregunta (coordenada de la recurrencia superior)
-	if (from == 0)
+	for (unsigned int i = 0; i < (*areas).size(); ++i)
 	{
-		count += check(map, ncols, nrows, col-1, row-1, 8, obj); // checks 1
-		count += check(map, ncols, nrows, col  , row-1, 7, obj); // checks 2
-		count += check(map, ncols, nrows, col+1, row-1, 6, obj); // checks 3
-		count += check(map, ncols, nrows, col-1, row  , 5, obj); // checks 4
-		count += check(map, ncols, nrows, col+1, row  , 4, obj); // checks 5
-		count += check(map, ncols, nrows, col-1, row+1, 3, obj); // checks 6
-		count += check(map, ncols, nrows, col  , row+1, 2, obj); // checks 7
-		count += check(map, ncols, nrows, col+1, row+1, 1, obj); // checks 8
-	}
-	else if(from < 4)
-	{
-		count += check(map, ncols, nrows, col-1, row+1, 3, obj); // checks 6
-		count += check(map, ncols, nrows, col  , row+1, 2, obj); // checks 7
-		count += check(map, ncols, nrows, col+1, row+1, 1, obj); // checks 8
-		if (from == 1)
+		for (unsigned int j = 0; j < (*areas)[i].size(); ++j)
 		{
-			count += check(map, ncols, nrows, col+1, row-1, 6, obj); // checks 3
-			count += check(map, ncols, nrows, col+1, row  , 4, obj); // checks 5
-		}
-		else if (from == 3)
-		{
-			count += check(map, ncols, nrows, col-1, row-1, 8, obj); // checks 1
-			count += check(map, ncols, nrows, col-1, row  , 5, obj); // checks 4
-		}
-	}
-	else if (from == 4)
-	{	
-		count += check(map, ncols, nrows, col+1, row-1, 6, obj); // checks 3
-		count += check(map, ncols, nrows, col+1, row  , 4, obj); // checks 5
-		count += check(map, ncols, nrows, col+1, row+1, 1, obj); // checks 8
-	}
-	else if (from == 5)
-	{	
-		count += check(map, ncols, nrows, col-1, row-1, 8, obj); // checks 1
-		count += check(map, ncols, nrows, col-1, row  , 5, obj); // checks 4
-		count += check(map, ncols, nrows, col-1, row+1, 3, obj); // checks 6
-	}
-	else
-	{	
-		count += check(map, ncols, nrows, col-1, row-1, 8, obj); // checks 1
-		count += check(map, ncols, nrows, col  , row-1, 7, obj); // checks 2
-		count += check(map, ncols, nrows, col+1, row-1, 6, obj); // checks 3
-		if (from == 6)
-		{
-			count += check(map, ncols, nrows, col+1, row  , 4, obj); // checks 5
-			count += check(map, ncols, nrows, col+1, row+1, 1, obj); // checks 8
-		}
-		else if (from == 8)
-		{
-			count += check(map, ncols, nrows, col-1, row  , 5, obj); // checks 4
-			count += check(map, ncols, nrows, col-1, row+1, 3, obj); // checks 6
+			if ((*areas)[i][j] == toReplace)
+			{
+				(*areas)[i][j] = currentPoint;
+			}
 		}
 	}
 
-	return count;
 }
